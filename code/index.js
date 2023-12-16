9021678737357e23632e04d1b9696a8a779ebc01
Happy7 <58347721+Happyuky7@users.noreply.github.com>
2023-12-16 08:10:15 -0300

    Update index.js

--- code/index.js
+++ code/index.js
@@ -1,3 +1,14 @@
+
+/*
+  Author: Happyuky7
+  Github: https://github.com/Happyuky7/
+  Twitter: https://twitter.com/Happyuky7
+  Github Repository this project: https://github.com/Happyuky7/testnodemon1/
+  License: https://github.com/Happyuky7/testnodemon1/blob/master/LICENSE.txt
+  Version: 1.0.0
+  Description: This script is used to run nodemon and autorestart for crash.
+*/
+
 const express = require('express');
 const app = express();
 
